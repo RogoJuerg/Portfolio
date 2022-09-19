7c053c36911f3ea176fa6b4dd6de6613d7e03ae3
RogoJuerg <juergen.rogozin@gmail.com>
2022-09-19 23:02:39 +0200

    finished title section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TopInfoComponent } from './top-info/top-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopInfoComponent
   ],
   imports: [
     BrowserModule,
