5277ef55493c7f6197bfb7a80620a2bc99a73d7f
RogoJuerg <juergen.rogozin@gmail.com>
2022-09-29 03:14:32 +0200

    added filter function for project section

--- src/app/my-work-section/my-work-section.component.ts
+++ src/app/my-work-section/my-work-section.component.ts
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-work-section.component.scss']
 })
 export class MyWorkSectionComponent implements OnInit {
+  projects: Array<any> = [
+    {
+      name : 'Lieferheroes',
+      programming : ['../../assets/img/icons/javascript.png', '../../assets/img/icons/htmlcss.png'],
+      appImg : '../../assets/img/App-Showcase-lieferheroes.png',
+      linkGit : 'https://github.com/RogoJuerg/Join',
+      linkPage : 'https://juergen-rogozin.developerakademie.net/Lieferando/index.html',
+      description : 'A clone of a popular food delivery service in germany written in vanilla JavaScript',
+      language : 'javascript'
+    },
+    {
+      name : 'JOIN',
+      programming : ['../../assets/img/icons/javascript.png', '../../assets/img/icons/htmlcss.png'],
+      appImg : '../../assets/img/App-Showcase-Join.png',
+      linkGit : 'https://github.com/RogoJuerg/Join',
+      linkPage : 'https://gruppe-256.developerakademie.net/index.html',
+      description : 'A kan ban board to create and manage tickets. It is written in vanilla JavaScript. Worked in a group project',
+      language : 'javascript'
+    },
+    {
+      name : 'ANGULARRR',
+      programming : ['../../assets/img/icons/javascript.png', '../../assets/img/icons/htmlcss.png'],
+      appImg : '../../assets/img/App-Showcase-Join.png',
+      linkGit : 'https://github.com/RogoJuerg/Join',
+      linkPage : 'https://gruppe-256.developerakademie.net/index.html',
+      description : 'A kan ban board to create and manage tickets. It is written in vanilla JavaScript. Worked in a group project',
+      language : 'angular'
+    }
+  ];
+
+  sortBy: string = 'angular';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  filterFunk() {
+    var result: any = this.projects.filter(e => e.language == this.sortBy)
+    console.log(result);
+    if(this.sortBy == 'all') {
+      return this.projects;
+    }
+    return result;
+  }
+
+  changeSort(filter: string) {
+    this.sortBy = filter;
+    console.log(this.sortBy);
   }
 
 }
